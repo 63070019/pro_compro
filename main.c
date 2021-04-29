0e9e80d38c4bc866ea46141c9669243a6809e40a
63070028 <71110346+63070028@users.noreply.github.com>
2021-04-29 14:24:00 +0700

    full.version

--- main.c
+++ main.c
@@ -22,13 +22,12 @@ struct goods
 
 
 
-void main_menu(struct goods item[]);
-
+int main_menu(struct goods item[]);
 
 void manager_menu(struct goods item[]);
 void check_item();
 void see_itme(struct goods item[]);
-void add_item(struct goods item[]);
+void add_products(struct goods item[]);
 void stock(struct goods item[]);
 void change(struct goods item[]);
 void edit_item(struct goods item[]);
@@ -36,11 +35,18 @@ void delete_item(struct goods item[]);
 void search_item(struct goods item[]);
 
 
+
+
+
 void guest_menu(struct goods item[]);
 
-void cal_user(struct goods item[]);
-void select_item(struct goods item[]);
+void select_item(struct goods item[], double result);
+void view_item(struct goods item[]);
+void add_item(struct goods item[], double result);
+void reduce_item(struct goods item[], double result);
+void check_discout(struct goods item[]);
 void bill(struct goods item[], double result);
+void reset_discount(struct goods item[]);
 
 
 
@@ -49,19 +55,27 @@ int main(){
     main_menu(item);
 }
 
-void main_menu(struct goods item[]){
-    printf("\tmenu\n");
+int main_menu(struct goods item[]){
+    int state;
+    printf("\n\n\t\t\t\tmenu\n\n");
     printf("\t1.guest\n");
     printf("\t2.manager\n");
-
-
-
-    int i;
-    scanf("%d", &i);
-    if(i == 1){
+    scanf("%d", &state);
+    while (state != 1 && state != 2)
+    {
+        printf("\n\n\t\t\t\tmenu\n\n");
+        printf("\t1.guest\n");
+        printf("\t2.manager\n");
+        scanf("%d", &state);
+        if(state == 1 || state == 2){
+            break;
+        }
+    }
+    
+    if(state == 1){
         guest_menu(item);
     }
-     else if (i == 2){
+    else if (state == 2){
         manager_menu(item);
     }
 }
@@ -69,12 +83,12 @@ void main_menu(struct goods item[]){
 
 void manager_menu(struct goods item[]){
     int state;
-    printf("\tWelcome Manager\n");
+    printf("\n\n\t\t\t\tWelcome Manager\n\n");
     printf("\t1.ดูสินค้าทั้งหมด\n");
     printf("\t2.เพิ่มสินค้า\n");
     printf("\t3.แก้ไขสินค้า\n");
     printf("\t4.ค้นหาสินค้า\n");
-    printf("\t0.ย้แนกลับ\n");
+    printf("\t0.ย้อนกลับ\n");
 
 
     scanf("%d", &state);
@@ -82,7 +96,7 @@ void manager_menu(struct goods item[]){
         see_itme(item);
     }
     else if (state == 2){
-        add_item(item);
+        add_products(item);
     }
     else if (state == 3){
         edit_item(item);
@@ -99,10 +113,11 @@ void manager_menu(struct goods item[]){
 
 
 
-void add_item(struct goods item[]){
+void add_products(struct goods item[]){
     int i;
     int get;
     char t[50];
+    printf("\n\n\t\t\t\tเพิ่มสินค้า\n\n");
     printf("\tกรุณาป้อนจำนวนสินค้าที่เพิ่ม:  ");
     scanf("%d", &get);
     for(int j=count_item; j<count_item+get; j++){
@@ -130,8 +145,8 @@ void add_item(struct goods item[]){
         item[j].dis_count = 0;
     }
     count_item += get;
-    printf("\n\tป้อนข้อมูลสำเร็จ !!!!!\n");
-    printf("\t0.ย้อนกลับ\n");
+    printf("\n\n\t\t\t\tป้อนข้อมูลสำเร็จ !!!!!\n\n");
+    printf("\n\t0.ย้อนกลับ\n");
     scanf("%d", &i);
     if(i == 0){
         manager_menu(item);
@@ -142,7 +157,7 @@ void add_item(struct goods item[]){
 
 void check_item(){
     if(count_item == 0){
-        printf("\tNo\t\tID\t\tName\t\tType\t\tPrice\n");
+        printf("\n\n\tNo\t\tID\t\tName\t\tType\t\tPrice\n\n");
         printf("\n\n\t\t\t\t\tไม่มีสินค้า\n\n");
     }
 }
@@ -153,7 +168,7 @@ void stock(struct goods item[]){
     check_item();
     for(int j=0; j<count_item; j++){
         if(j == 0){
-            printf("\tNo\t\tID\t\tName\t\tType\t\tPrice\t\tQuantity\n");
+            printf("\n\n\tNo\t\tID\t\tName\t\tType\t\tPrice\t\tQuantity\n\n");
         }
         printf("\t%d\t\t", item[j].no);
         printf("%s\t\t", item[j].id);
@@ -177,8 +192,9 @@ void see_itme(struct goods item[]){
 
 void edit_item(struct goods item[]){
     int state;
+    printf("\n\n\t\t\t\tแก้ไขสินค้า\n\n");
     stock(item);
-    printf("\n\t1.ต้องการเปลี่ยนข้อมูล\n");
+    printf("\n\n\n\t1.ต้องการเปลี่ยนข้อมูล\n");
     printf("\t2.ต้องการลบข้อมูล\n");
     printf("\t0.ย้อนกลับ\n");
 
@@ -204,13 +220,14 @@ void change(struct goods item[]){
     for(int j=0; j<count_item; j++){
         if(j == num-1){
             while (ation)
-            {               
-                printf("\n\t1.ID:  \n");
-                printf("\t2.Name:  \n");
-                printf("\t3.Type:  \n");
-                printf("\t4.Price:  \n");
-                printf("\t5.Quantity:  \n");
-                printf("\t0.ย้อนกลับ:  \n");
+            {   
+                stock(item);    
+                printf("\n\t1.ID\n");
+                printf("\t2.Name\n");
+                printf("\t3.Type\n");
+                printf("\t4.Price\n");
+                printf("\t5.Quantity\n");
+                printf("\t0.ย้อนกลับ\n");
                 scanf("%d", &state);
                 if(state == 1){
                     printf("\n\tID:  ");
@@ -239,7 +256,7 @@ void change(struct goods item[]){
                     ation = false;
                 }
                 if(state !=  0){
-                    printf("\tแก้ไขสำเร็จ  !!!\n");
+                    printf("\n\tแก้ไขสำเร็จ  !!!\n\n");
                 }
                 
             }
@@ -288,74 +305,139 @@ void search_item(struct goods item[]){
     }
 }
 
+
+
+
+
+
+
 void guest_menu(struct goods item[]){
-    printf("\t\tWelcome To Shop\n");
-    select_item(item);
+    printf("\n\n\t\t\t\t\t\tWelcome To Shop\n\n\n");
+    double result = 0;
+    reset_discount(item);
+    select_item(item, result);
 }
 
 
-
-
-
-void select_item(struct goods item[]){
+void select_item(struct goods item[], double result){
     bool ation = true;
-    double result = 0;
     while (ation)
     {
         stock(item);
         printf("\n\n\tยอมรวม:\t%.2lf\n", result);
-        int i;
         int state;
-        int num;
-        printf("\t1.เลือกซื้อสินค้า\n");
-        printf("\t0.ชำระเงิน\n");
+        printf("\t1.เพิ่มสินค้า\n");
+        printf("\t2.ลดสินค้า\n");
+        printf("\t3.ชำระเงิน\n");
         scanf("%d", &state);
         if(state == 1){
-            printf("\tป้อนสินค้าที่ต้อง No:\t");
-            scanf("%d", &i);
-            for(int j=0; j<count_item; j++){
-                if(i-1 == j){
-                    printf("\t%s\tจำนวน:\t", item[j].name);
-                    scanf("%d", &num);
-                    while (num>item[j].quantity){
-                        printf("\tมีสินค้าไม่เพียงพอกรุณาป้อนใหม่\n\t0.ยกเลิก\n");
-                        printf("\t%s\tจำนวน:\t", item[j].name);
-                        scanf("%d", &num);
-                        if(num == 0){
-                            select_item(item);
-                        }
-                    }
-                    result += item[j].price*num;
-                    item[j].quantity = item[j].quantity - num;
-                    item[j].dis_count += num;
-                }
-            }
+            add_item(item, result);
         }
-        else if (state == 0){
+        else if (state == 2){
+           reduce_item(item, result);
+        }
+        else if (state == 3){
            bill(item, result);
         }
     }
 }
 
 
+void view_item(struct goods item[]){
+    printf("\n\n\tNo\tสินค้า\tจำนวน\tราคา\n\n");
+    for(int j=0; j<count_item; j++){
+        if(item[j].dis_count > 0){
+            printf("\t%d\t%s\tx%d\t%.2lf\n", item[j].no, item[j].name, item[j].dis_count, item[j].dis_count*item[j].price);
+        }
+    }
+}
+
+void add_item(struct goods item[], double result){
+    int i;
+    int num;
+    printf("\n\tป้อนสินค้าที่ต้องเพิ่ม No:\t");
+    scanf("%d", &i);
+    for(int j=0; j<count_item; j++){
+        if(i == item[j].no){
+            printf("\t%s\tจำนวน:\t", item[j].name);
+            scanf("%d", &num);
+            while (num>item[j].quantity){
+                printf("\tมีสินค้าไม่เพียงพอกรุณาป้อนใหม่\n\t0.ยกเลิก\n");
+                printf("\t%s\tจำนวน:\t", item[j].name);
+                scanf("%d", &num);
+                if(num == 0){
+                    select_item(item, result);
+                }
+            }
+            result += item[j].price*num;
+            item[j].quantity = item[j].quantity - num;
+            item[j].dis_count += num;
+        }
+    }
+    select_item(item, result);
+}
+
+void check_discout(struct goods item[]){
+    int i = 0;
+    for(int j=0; j<count_item; j++){
+        if(item[j].dis_count == 0){
+            i++;
+        }
+    }
+    if(i == count_item){
+        printf("\n\t\tไม่มีสินค้า\n\n");
+        printf("\n\t0.ย้อนกลับ\n");
+    }
+}
+
+
+
+void reduce_item(struct goods item[], double result){
+    int i;
+    int num;
+    view_item(item);
+    check_discout(item);
+    printf("\n\tป้อนสินค้าที่ต้องลด No:\t");
+    scanf("%d", &i);
+    for(int j=0; j<count_item; j++){
+        if(i == item[j].no){
+            printf("\t%s\tจำนวน:\t", item[j].name);
+            scanf("%d", &num);
+            while (num>item[j].dis_count){
+                printf("\tมีสินค้าไม่เพียงพอกรุณาป้อนใหม่\n\t0.ยกเลิก\n");
+                printf("\t%s\tจำนวน:\t", item[j].name);
+                scanf("%d", &num);
+                if(num == 0){
+                    select_item(item, result);
+                }
+            }
+            result -= item[j].price*num;
+            item[j].quantity = item[j].quantity + num;
+            item[j].dis_count -= num;
+        }
+    }
+    select_item(item, result);
+}
+
 
 
 void bill(struct goods item[], double result){
     int state;
-    printf("\n\n");
-    printf("\tสินค้า\tจำนวน\tราคา\n");
-    for(int j=0; j<count_item; j++){
-        if(item[j].dis_count > 0){
-            printf("\t%s\tx%d\t%.2lf\n", item[j].name, item[j].dis_count, item[j].dis_count*item[j].price);
-            item[j].dis_count = 0;
-        }
-    }
-
+    view_item(item);
     printf("\n\n\tจำนวนเงินที่ต้องชำระ: %.2lf\n\n", result);
     printf("\t0.กลับไปหน้าแรก\n");
     scanf("%d", &state);
     if(state == 0){
+        result = 0;
         main_menu(item);
     }
 
 }
+
+void reset_discount(struct goods item[]){
+    for(int j=0; j<count_item; j++){
+        item[j].dis_count = 0;
+    }
+}
+
+
